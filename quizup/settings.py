bb97ac85f85b333ee5929028b63d2c2e7f709565
zayonsoft <favourlosotu@gmail.com>
2025-11-27 23:36:46 +0100

    Added DB Password

--- quizup/settings.py
+++ quizup/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "quizup",
         "USER": "quizup_user",
-        "PASSWORD": "YourStrongPassword",
+        "PASSWORD": config("DB_PASSWORD"),
         "HOST": "localhost",
         "PORT": "5432",
     }
